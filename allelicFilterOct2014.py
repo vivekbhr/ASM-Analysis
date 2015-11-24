94f1c0ac77563491f3bbe3dd9388d5f4d9325d04
vivekbhr <bhardwaj@ie-freiburg.mpg.de>
2015-11-24 17:02:53 +0100

    sorting is default, only sorted file is kept

--- allelicFilterOct2014.py
+++ allelicFilterOct2014.py
@@ -31,12 +31,12 @@ def get_args():
     parser.add_argument('--filterDuplicates', '-dup', type=str, default='yes', help = 'remove duplicates; default: yes')
     parser.add_argument('--filterForMappingQuality', '-mapQ', type=int, help = 'indicate a minimum mapping quality that each read (pair) should have, default: 0')
     parser.add_argument('--removeMultiMapped', '-vMulti', action='store_true', default=False, help = 'exclude reads that have more than one alignment based on the NH and/or XS tag; default: not set')
-    parser.add_argument('--coordinateSorting', '-coordSort', action='store_true', default=False, help = 'sort the resulting bam files according to read coordinates and index them; default: not set')
+    parser.add_argument('--coordinateSorting', '-coordSort', action='store_true', default=True, help = 'sort the resulting bam files according to read coordinates and index them; default: not set')
 
     args=parser.parse_args()
     return args
 
-    
+
 def prepare_header(BamFile, Sorting):
     '''replaces the entry that indicates the sorting of a BAM file with a PICARD-consistent entry'''
     replacements = {'query' : 'queryname', 'unsort' : 'unsorted' , 'coordinate' : 'coordinate'}
@@ -49,24 +49,24 @@ def prepare_header(BamFile, Sorting):
         for k in headerList:
             if re.search(k, oH['HD']['SO']):
                 oH['HD']['SO'] = replacements[k]
-    
+
     return(oH)
 
 
 def remove_multiAlignments(Read, KeepInfo):
     '''Checks for NH and XS tag to remove reads that aligned more than once,
     returns a boolean whether the read should be kept or not
-    '''    
+    '''
     keepRead = KeepInfo
-   
+
     # the highest possible alignment score in end-to-end align.bamment is 0.
     # For each mismatch or gap open/extension,
     # that score is decremented by some amount --> negative values
     try:
-        alignScore = [item for item in Read.tags if item[0] == 'AS'][0][1] 
+        alignScore = [item for item in Read.tags if item[0] == 'AS'][0][1]
     except IndexError:
-        alignScore = None        
-    
+        alignScore = None
+
     # XS --> tophat uses it to indicate which strand the read originated from (+/-)
     # the XS flag is set by bowtie2, when a second valid alignment for the same read is found
     # if the alignment score of the reported read AS==XS,
@@ -76,36 +76,36 @@ def remove_multiAlignments(Read, KeepInfo):
         additionalAlignScore = [item for item in Read.tags if item[0] == 'XS'][0][1]
     except IndexError:
         additionalAlignScore = None
-    
+
     # NH:i tag should indicate how many alignments were found
     # it is not set by bowtie2 or bwa
     # not sure if this is a good filter
     try:
         alignNumbers = [item for item in Read.tags if item[0] == 'NH'][0][1]
     except IndexError:
-        alignNumbers = None 
-    
-    
+        alignNumbers = None
+
+
     if isinstance(additionalAlignScore, (int, long, float, complex)) and alignScore == additionalAlignScore:
         keepRead=False
     elif alignNumbers > 1:
         keepRead=False
-        
+
     return(keepRead)
-    
+
 def check_mappingQuality(Read, KeepInfo, MinMapQ):
     '''Returns boolean based on the mapping quality filter indicated by the user'''
     keepRead = KeepInfo
     if Read.mapq < MinMapQ:
         keepRead = False
-    
+
     return(keepRead)
-        
-        
+
+
 def save_reads(Read, KeepInfo, outFile1, outFile2, outFile3, c):
     '''Checks the origin of a read and saves it to the respective file'''
     ReadOrigin = [item for item in Read.tags if item[0] == 'po'][0][1]
-    
+
     if(ReadOrigin == 1 and KeepInfo):
             outFile1.write(Read)
             c[0] += 1
@@ -121,12 +121,11 @@ def save_reads(Read, KeepInfo, outFile1, outFile2, outFile3, c):
 
 def sort_output(outPrefix):
     '''Sorts the output file by read coordinate'''
-    pysam.sort(outPrefix+'.originalSort.bam', outPrefix + '.coordSort')    
-    #os.remove(outPrefix+'.originalSort.tmp.bam')
-    
-    ## Build the bam index for output    
+    pysam.sort(outPrefix+'.originalSort.bam', outPrefix + '.coordSort')
+    ## Build the bam index for output
     pysam.index(outPrefix + '.coordSort.bam')
-    
+    ## Remove original sorted file
+    os.remove(outPrefix+'.originalSort.bam')
 
 def main():
 
@@ -136,9 +135,9 @@ def main():
     args = get_args()
 
     infile = pysam.Samfile(args.BAMfile, "rb")
-    
+
     newHeader = prepare_header(infile, args.coordinateSorting)
-    
+
     out1 = args.outfile1
     out2 = args.outfile2
     out1_noSort = pysam.Samfile(out1 + '.originalSort.bam', "wb", header = newHeader ) # template will cause the original header to be used, one could modify the header by generating an appropriate dictionary structure, newHeader, and then use that: header = newHeader
@@ -146,10 +145,10 @@ def main():
     if args.outfile3:
         out3 = args.outfile3
         out3_noSort = pysam.Samfile(out3 + '.originalSort.bam' , "wb", header = newHeader )
-     
+
     else:
         out3 = None
-    
+
     keep = True
     origin = 0
     Counts = [0,0,0] # keeping track of reads per origin
@@ -159,21 +158,21 @@ def main():
     # that when true, it means to keep the read
     seen = {}
     seenRead = {}
-    
+
     for DNAread in infile:
         #check if read should be kept
         if DNAread.flag == 4: # excluding unmapped reads
-            keep = False    
+            keep = False
 
         if args.filterDuplicates == 'yes' and DNAread.is_duplicate:
             keep=False
 
         if keep and [item for item in DNAread.tags if item[0] == 'ct'][0][1] == 'R':
             keep = False
-        
+
         if keep and args.removeMultiMapped:
             keep = remove_multiAlignments(DNAread, True)
-        
+
         if keep and args.filterForMappingQuality:
             keep = check_mappingQuality(DNAread, True, args.filterForMappingQuality)
 
@@ -189,7 +188,7 @@ def main():
                 Counts = save_reads(seenRead[DNAread.qname], True, outFile1 = out1_noSort, outFile2 = out2_noSort, outFile3 = out3_noSort, c = Counts)
             del(seen[DNAread.qname])
             del(seenRead[DNAread.qname])
-            
+
         # if the current read is not within the seen dictionary
         # first check if the read is unpaired
         # if unpaired, save and forget
@@ -206,7 +205,7 @@ def main():
     for entry in seen:
         Counts = save_reads(seenRead[entry], seen[entry], outFile1 = out1_noSort, outFile2 = out2_noSort, outFile3 = out3_noSort, c = Counts)
         keep = True
-        
+
     out1_noSort.close()
     out2_noSort.close()
     if args.outfile3:
@@ -214,7 +213,7 @@ def main():
 
     CountOut="reads mapped to alternative genome 1 ({1}): {0}\nreads mapped to alternative genome 2 ({3}): {2}\nundistinguishable reads {4}".format(Counts[0], args.outfile1, Counts[1], args.outfile2, Counts[2])
     print CountOut
-    
+
     if args.coordinateSorting:
         sort_output(out1)
         sort_output(out2)
@@ -223,4 +222,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
